0701b35fbbc5d8abb530e864555aacae0370c4f5
Bianca Lungu <Bianca.Lungu@endava.com>
2026-04-28 11:14:54 +0300

    save main.py changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 from app.config import settings
 from app.database import create_db_and_tables
 from app import models
-
+from app.manager_statistics.router import router as manager_statistics_router
 
 app = FastAPI(title="ChampionsClub API")
+
+app.include_router(manager_statistics_router)
 
 
 @app.on_event("startup")
